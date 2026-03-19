5dd8920d9e737135217cd61fed623c8c389383b7
Eduardo <dj.shadowjeab@gmail.com>
2026-03-18 22:39:18 -0500

    introjs

---
+++ 02-JAVASCRIPT/capitulo-01/app-01.js
@@ -0,0 +1,29 @@
+/*
+    JAVASCRIPT
+
+    - Es un lenguaje de programación
+    - Se ejecuta del lado del cliente 
+    - JS es key sensitive (diferencia entre mayúsculas y minúsculas)
+    - el punto y como no es obligatorio, pero es buena práctica usarlo para separar las instrucciones
+    - JS es un lenguaje basado en objetos, lo que significa que todo en JS es un objeto, incluso las funciones
+    - No es de tipado fuerte
+    - JS se basa y manipula el DOM (Document Object Model) para interactuar con el HTML y CSS
+    - la ultima version JS es ES2024, pero la mayoría de los navegadores soportan ES6 (ECMAScript 2015) y versiones posteriores
+*/
+
+// TIPOS DE DATOS EN JAVASCRIPT
+
+// 1. String (cadena de texto)
+// let y const
+//  el signo igual es de asignación, no de comparación
+let nombre = "Eduardo";
+let apellido = 'García';
+const anioNacimiento = 'nací en el año 1990';
+console.log(anioNacimiento)
+
+
+// 2. Number (número)
+// 3. Boolean (booleano)
+// 4. Null (nulo)
+// 5. Undefined (indefinido)
+// 6. Object (objeto) ⚡⚡⚡
