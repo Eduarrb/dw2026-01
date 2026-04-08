c6528119504e3b338a699e59763eefcc1de98312
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2026-04-08 09:41:15 -0500

    cart

--- 01-HTML-CSS/04-proyecto/js/codigo.js
+++ 01-HTML-CSS/04-proyecto/js/codigo.js
@@ -1,7 +1,8 @@
-const circle  = document.querySelector('.nav__contenedor__menuIcon__circle');
+const circle = document.querySelector('.nav__contenedor__menuIcon__circle');
 const menu = document.querySelector('.nav__contenedor__menu');
 const menuIcon = document.querySelector('.nav__contenedor__menuIcon');
 const filtrosTag = document.querySelector('.contenidoProd__contenedor__filtrosTags');
+const inputSearch = document.querySelector('.contenidoProd__contenedor__form__inputSearch input');
 
 menuIcon.addEventListener('click', () => {
 	menu.classList.toggle('active');
@@ -25,24 +26,23 @@ inputSelects.forEach((input) => {
 	});
 
 	ul.addEventListener('click', (e) => {
-        if (e.target.tagName === 'LI') {
-            const listas = ul.querySelectorAll('li');
-            listas.forEach((li) => {
-                const icon = li.querySelector('i');
-                if (icon) icon.remove();
-            });
+		if (e.target.tagName === 'LI') {
+			const listas = ul.querySelectorAll('li');
+			listas.forEach((li) => {
+				const icon = li.querySelector('i');
+				if (icon) icon.remove();
+			});
 			e.target.innerHTML += '<i class="fa-solid fa-check"></i>';
-            e.target.parentElement.parentElement.firstElementChild.innerHTML = `${e.target.textContent} <i class="fa-solid fa-angle-down"></i>`;
-			if(e.target.parentElement.parentElement.classList.contains('contenidoProd__contenedor__form__todos')) {
-				if(e.target.textContent === 'Todas') {
-					if(filtrosTag.firstChild) {
+			e.target.parentElement.parentElement.firstElementChild.innerHTML = `${e.target.textContent} <i class="fa-solid fa-angle-down"></i>`;
+			if (e.target.parentElement.parentElement.classList.contains('contenidoProd__contenedor__form__todos')) {
+				if (e.target.textContent === 'Todas') {
+					if (filtrosTag.firstChild) {
 						filtrosTag.firstChild.remove();
 					}
-				}
-				else {
-					if(filtrosTag.firstChild) {
+				} else {
+					if (filtrosTag.firstChild) {
 						filtrosTag.firstChild.remove();
-					}	
+					}
 					const span = document.createElement('span');
 					span.innerHTML = e.target.textContent + ' <i class="fa-solid fa-x"></i>';
 					filtrosTag.prepend(span);
@@ -52,3 +52,28 @@ inputSelects.forEach((input) => {
 		}
 	});
 });
+
+inputSearch.addEventListener('input', () => {
+	const value = inputSearch.value.trim();
+	if (!filtrosTag.querySelector('.inputTag')) {
+		const span = document.createElement('span');
+		span.classList.add('inputTag');
+		span.innerHTML = value + ' <i class="fa-solid fa-x"></i>';
+		filtrosTag.append(span);
+	} else {
+		if (value === '') {
+			filtrosTag.querySelector('.inputTag').remove();
+			return;
+		} {
+			const tag = filtrosTag.querySelector('.inputTag');
+			tag.innerHTML = value + ' <i class="fa-solid fa-x"></i>';
+		}
+	}
+});
+
+filtrosTag.addEventListener('click', (e) => {
+	console.log(e)
+	if(e.target.tagName === 'SPAN') {
+		e.target.remove();
+	}
+})
